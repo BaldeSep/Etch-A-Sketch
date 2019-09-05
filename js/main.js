7d5c7feb0fe2e537592229ba81e620f37e842abf
Baldemar Sepulveda <balde.sepu@pm.me>
2019-09-04 19:25:16 -0700

    Can now change grid size and reset grid

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
 let defaultSquareColor = "#05204a";
 let color = "#fff";
 
-let state = "Erase";
+let state = "Color";
 
-const DEFAULT_GRID_SIZE = 30;
+const DEFAULT_GRID_SIZE = 16;
 
 let gridSize = DEFAULT_GRID_SIZE;
 
 const gridWrapperElement = document.getElementById("grid-wrapper");
-gridWrapperElement.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+
+const gridSizeModal = document.querySelector(
+  ".modal[data-purpose='grid-size']"
+);
+
+const gridSizeInput = document.getElementById("input-number-modal-size");
+
+const gridSizeModalOkButton = gridSizeModal.children[0].children[2].children[1];
+const gridSizeModalCancelButton =
+  gridSizeModal.children[0].children[2].children[0];
+
+gridSizeModalOkButton.addEventListener("click", e => {
+  const newSizeInputValue = gridSizeInput.value;
+  if (newSizeInputValue > 0 && newSizeInputValue < 150) {
+    createNewGrid(newSizeInputValue, true);
+    gridSizeModal.style.display = "none";
+  }
+});
+
+const newGridButton = document.getElementById("set-grid-size");
+newGridButton.addEventListener("click", e => {
+  gridSizeModal.style.display = gridSizeModal.dataset.display;
+});
 
 const eraserButton = document.getElementById("eraser");
 eraserButton.addEventListener("click", e => {
@@ -21,18 +43,13 @@ colorButton.addEventListener("click", e => {
 });
 
 const clearGridButton = document.getElementById("clear-grid");
-clearGridButton.addEventListener("click", e => {
+clearGridButton.addEventListener("click", clearGrid);
+
+function clearGrid(e) {
   const allSquares = gridWrapperElement.children;
   for (let i = 0; i < allSquares.length; i++) {
     allSquares[i].style.backgroundColor = defaultSquareColor;
   }
-});
-
-let square;
-for (let i = 0; i < gridSize ** 2; i++) {
-  square = document.createElement("div");
-  square.addEventListener("mouseover", alterColor);
-  gridWrapperElement.appendChild(square);
 }
 
 function alterColor(e) {
@@ -47,3 +64,24 @@ function alterColor(e) {
 function setState(newState) {
   state = newState;
 }
+
+function createNewGrid(newGridSize = DEFAULT_GRID_SIZE, reset = false) {
+  gridWrapperElement.style.gridTemplateColumns = `repeat(${newGridSize}, 1fr)`;
+  if (reset) {
+    let currentChild = gridWrapperElement.lastElementChild;
+    while (currentChild) {
+      gridWrapperElement.removeChild(currentChild);
+      currentChild = gridWrapperElement.lastElementChild;
+    }
+  }
+
+  gridSize = newGridSize;
+  let square;
+  for (let i = 0; i < gridSize ** 2; i++) {
+    square = document.createElement("div");
+    square.addEventListener("mouseover", alterColor);
+    gridWrapperElement.appendChild(square);
+  }
+}
+
+createNewGrid();
